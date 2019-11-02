8275a5f320de49ebcaf187da4cac5f20e70afb76
MarcelIwanicki <iwanickimarcelgames@gmail.com>
2019-11-02 11:44:46 +0100

    Friend's posts now show on welcome board

--- src/main/java/com/sharing/overload/controller/RestAppPostController.java
+++ src/main/java/com/sharing/overload/controller/RestAppPostController.java
@@ -1,6 +1,8 @@
 package com.sharing.overload.controller;
 
 import com.sharing.overload.entity.AppPost;
+import com.sharing.overload.entity.AppUser;
+import com.sharing.overload.service.AppFriendsService;
 import com.sharing.overload.service.AppPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,9 +20,25 @@ public class RestAppPostController {
     @Autowired
     private AppPostService appPostService;
 
+    @Autowired
+    private AppFriendsService appFriendsService;
+
     @GetMapping("/{username}")
     public List<AppPost> getUserPosts(@PathVariable String username) {
         return appPostService.findAppPostByUsername(username);
+    }
+
+    @GetMapping("/friends/{username}")
+    public List<AppPost> getUsersFriendsPosts(@PathVariable String username) {
+        List<AppPost> result = new ArrayList<>();
+
+        List<AppUser> friends = appFriendsService.getFriends(username);
+        for (AppUser f : friends) {
+            List<AppPost> posts = appPostService.findAppPostByUsername(f.getUsername());
+            result.addAll(posts);
+        }
+
+        return result;
     }
 
     @GetMapping("/id/{id}")
